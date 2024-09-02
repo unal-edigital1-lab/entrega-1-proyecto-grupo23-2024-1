5dfa9b82a9ee3e03d5bcbd896d855d8a15c95c52
Kevinguerra54 <kevincifuentes1001@gmail.com>
2024-09-02 05:34:54 -0500

    fsm2

--- db/fsm.v
+++ db/fsm.v
@@ -5,153 +5,116 @@ module fsm (
     input descansar,
     input test,
     input clk,
-	 input acelerar,
+    input acelerar,
     output reg [2:0] estado
 );
 
-wire [5:0]sec;
+wire [5:0] sec;
 wire [5:0] min;
 wire [4:0] hour;
 
-tiempo mi_tiempo(.clk(clk),.rst(rst),.sec(sec),.min(min),.hour(hour),.A(acelerar));
-    
+tiempo mi_tiempo(.clk(clk), .rst(rst), .sec(sec), .min(min), .hour(hour), .A(acelerar));
+
     parameter MUERTO = 3'b000;
-    parameter FELIZ1 = 3'b001;
-    parameter FELIZ2 = 3'b010;
-    parameter FELIZ3 = 3'b011;
-    parameter FELIZ4 = 3'b100;
-    parameter ABURRIDO1 = 3'b101;
-    parameter ABURRIDO2 = 3'b110;
-    parameter ABURRIDO3 = 3'b111;
-    parameter TRISTE1 = 3'b000;
-    parameter TRISTE2 = 3'b001;
-    parameter TRISTE3 = 3'b010;
-    parameter NORMAL1 = 3'b011;
-    parameter NORMAL2 = 3'b100;
-    parameter NORMAL3 = 3'b101;
-    parameter ENFERMO = 3'b110;
+    parameter FELIZ = 3'b001;
+    parameter ABURRIDO = 3'b010;
+    parameter TRISTE = 3'b011;
+    parameter NORMAL = 3'b100;
+    parameter ENFERMO = 3'b101;
+    parameter CANSADO = 3'b110;
+    parameter HAMBRIENTO = 3'b111;
 
     reg [2:0] current_state, next_state;
+    reg [4:0] ultima_actividad;
 
-    
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            current_state <= MUERTO;
+            current_state <= NORMAL;
+            ultima_actividad <= hour;
         end else begin
             current_state <= next_state;
+            if (comer || jugar || descansar) begin
+                ultima_actividad <= hour;
+            end
         end
     end
 
-    
     always @(*) begin
         next_state = current_state;
 
         case (current_state)
             MUERTO: begin
-                if (test) begin
-                    next_state = NORMAL1;
+                if (comer || jugar || descansar) begin
+                    next_state = NORMAL;
                 end
             end
 
-            FELIZ1: begin
-                if (comer) begin
-                    next_state = FELIZ2;
-                end else if (jugar) begin
-                    next_state = FELIZ3;
-                end
-            end
-            FELIZ2: begin
-                if (comer) begin
-                    next_state = FELIZ3;
-                end else if (jugar) begin
-                    next_state = FELIZ4;
-                end
-            end
-            FELIZ3: begin
-                if (comer) begin
-                    next_state = FELIZ4;
+            FELIZ: begin
+                if (hour - ultima_actividad > 2) begin
+                    next_state = ABURRIDO;
                 end else if (descansar) begin
-                    next_state = NORMAL1;
-                end
-            end
-            FELIZ4: begin
-                if (!comer && !jugar) begin
-                    next_state = NORMAL1;
+                    next_state = CANSADO;
                 end
             end
 
-            ABURRIDO1: begin
-                if (!jugar) begin
-                    next_state = ABURRIDO2;
-                end
-            end
-            ABURRIDO2: begin
-                if (!jugar) begin
-                    next_state = ABURRIDO3;
-                end
-            end
-            ABURRIDO3: begin
-                if (comer) begin
-                    next_state = FELIZ1;
+            ABURRIDO: begin
+                if (jugar) begin
+                    next_state = FELIZ;
+                end else if (hour - ultima_actividad > 4) begin
+                    next_state = TRISTE;
                 end else if (descansar) begin
-                    next_state = TRISTE1;
+                    next_state = CANSADO;
                 end
             end
 
-            TRISTE1: begin
-                if (!comer) begin
-                    next_state = TRISTE2;
-                end
-            end
-            TRISTE2: begin
-                if (!comer) begin
-                    next_state = TRISTE3;
-                end else if (jugar) begin
-                    next_state = FELIZ1;
-                end
-            end
-            TRISTE3: begin
-                if (!comer) begin
+            TRISTE: begin
+                if (jugar) begin
+                    next_state = FELIZ;
+                end else if (hour - ultima_actividad > 6) begin
                     next_state = MUERTO;
-                end else if (jugar) begin
-                    next_state = FELIZ1;
                 end
             end
 
-            NORMAL1: begin
+            NORMAL: begin
                 if (comer) begin
-                    next_state = FELIZ1;
+                    next_state = FELIZ;
                 end else if (!jugar) begin
-                    next_state = ABURRIDO1;
-                end
-            end
-            NORMAL2: begin
-                if (!jugar) begin
-                    next_state = ABURRIDO2;
-                end
-            end
-            NORMAL3: begin
-                if (descansar) begin
-                    next_state = ENFERMO;
+                    next_state = ABURRIDO;
+                end else if (descansar) begin
+                    next_state = CANSADO;
                 end
             end
 
             ENFERMO: begin
-                if (test) begin
-                    next_state = NORMAL1;
-                end else if (!comer && !jugar && !descansar) begin
+                if (hour - ultima_actividad > 3) begin
                     next_state = MUERTO;
                 end
             end
 
-            default: next_state = MUERTO;
+            CANSADO: begin
+                if (comer) begin
+                    next_state = HAMBRIENTO;
+                end else if (hour - ultima_actividad > 2) begin
+                    next_state = MUERTO;
+                end
+            end
+
+            HAMBRIENTO: begin
+                if (comer) begin
+                    next_state = NORMAL;
+                end else if (hour - ultima_actividad > 3) begin
+                    next_state = MUERTO;
+                end
+            end
+
+            default: next_state = NORMAL;
         endcase
     end
 
-   
     always @(posedge clk) begin
         estado <= current_state;
     end
 
 endmodule
+
 
